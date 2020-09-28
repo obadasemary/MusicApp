c1e5cc1424850a5c39b0c16a52e61d3635896fba
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-28 15:04:59 +0300

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Abdelrahman Mohamed on 28.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
