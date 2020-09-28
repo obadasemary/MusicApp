662aacbf7be8456ccea7d00a5004792e26e7ecd8
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-28 15:12:07 +0300

    Create MusicPlayerBar.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MusicApp/MusicPlayerBar.swift
@@ -0,0 +1,26 @@
+//
+//  MusicPlayerBar.swift
+//  MusicApp
+//
+//  Created by Abdelrahman Mohamed on 28.09.2020.
+//
+
+import SwiftUI
+
+struct MusicPlayerBar: View {
+    
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text("MUSIC PLAYER BAR")
+    }
+}
+
+struct MusicPlayerBar_Previews: PreviewProvider {
+    static var previews: some View {
+        MusicPlayerBar()
+    }
+}
